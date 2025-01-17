4d52b1de24342169fd5965531dc16e9a62b4b1fd
uwituzeb <b.uwituze@alustudent.com>
2025-01-17 12:11:25 +0200

    navbar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#333',
+        'dark-blue': '#0B132A',
+        'grey': '#4F5665'
+      },
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
